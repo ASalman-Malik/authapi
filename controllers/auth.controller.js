54cd86c702c208e1369310287cfaa5421649c323
Ahmad Salman Malik <36794358+ASalman-Malik@users.noreply.github.com>
2023-01-02 11:20:53 +0530

    jwt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -79,7 +79,7 @@ exports.signin = (req, res) => {
                 message : "Invalid Password"
             })
         }
-        var token = jw.sign({id:user.id}, config.secret,{
+        var token = jwt.sign({id:user.id}, config.secret,{
             expiresIn: 86400 //24 hours
         })
         var authorities = [];
